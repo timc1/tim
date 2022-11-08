8dc484cc877b4338a0279aa01cd648f0ba8aeb71
Tim <timothychang94@gmail.com>
2022-11-07 22:17:47 -0500

    chore: mastodon

--- app/root.tsx
+++ app/root.tsx
@@ -57,6 +57,7 @@ function Document({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="me" href="https://subculture.chat/@tim" title="Mastodon" />
         {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
